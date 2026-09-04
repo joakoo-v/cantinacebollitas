3cffd8d52bb09029e38f80dc970579788813f619
joakoo-v <cepedaviolajoaquinv@gmail.com>
2026-09-04 20:18:33 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,15 @@
+// ========================================
+// CONFIGURACIÓN DE SUPABASE
+// ========================================
 
+const SUPABASE_URL = "https://hmksgznuavtwwpsqqwms.supabase.co";
+
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_bTDBF4tTHtI2XhVHoTMK9g_zuhOwi9S";
+
+// Cliente de Supabase
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_PUBLISHABLE_KEY
+);
+
+console.log("Supabase conectado:", supabaseClient);
